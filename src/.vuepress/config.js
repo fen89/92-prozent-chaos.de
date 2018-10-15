1281ea820487bd2ca646387c5e8f00dc96796055
fen89 <tobias.goerg@googlemail.com>
2018-10-15 21:08:05 +0200

    Created reading-time skeleton

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -12,11 +12,12 @@ module.exports = {
   output: resolve(__dirname, 'dist'),
   plugins: [
     '@vuepress/blog',
-    '@vuepress/pagination',
+    // '@vuepress/pagination',
     '@vuepress/medium-zoom',
     "@vuepress/plugin-notification",
     "@vuepress/back-to-top",
-    // "./plugins/plugin-reading-time",
+    require('./plugins/plugins-responsive-images'),
+    require("./plugins/plugin-reading-time")
   ],
   permalink: ':year/:month/:day/:slug',
   serviceWorker: {
--- src/.vuepress/plugins/plugin-reading-time/index.js
+++ src/.vuepress/plugins/plugin-reading-time/index.js
@@ -1,10 +1,21 @@
-const path = require('path');
-
-module.exports = (pluginOptions = {}, context) => ({
+module.exports = (options, context) => ({
     name: 'my-reading-time',
-    extendPageData: ({ filePath, path, content, frontmatter }) {
+    extendPageData ({ content }) {
         return {
-            testitest: 'derp'
+            size: content ? ((content.length / 1024).toFixed(2) + 'kb') : 0
         }
+        // const { transformer } = options;
+        // const averageWordsPerMinute = 248;
+
+        // const text = _content;
+        // if (!text) return;
+
+        // const textLength = text.replace(/\s/g, '').length;
+        // const numberOfWords = textLength / averageWordsPerMinute;
+
+        // const time = Number(numberOfWords / averageWordsPerMinute);
+        // const readingTime = typeof transformer === 'function' ? transformer(time) : time;
+        
+        // return { readingTime };
     }
 });
